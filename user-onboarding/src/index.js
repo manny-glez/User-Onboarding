e6f7d0a4314cb00d6fdab52324e534be3aa5c10d
Emmanuel Gonzalez <dev.egonzalez1@gmail.com>
2021-07-22 16:32:30 -0500

    redeploy

--- user-onboarding/src/index.js
+++ user-onboarding/src/index.js
@@ -4,14 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter as Router } from 'react-router-dom'
-
 ReactDOM.render(
-  <Router>
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-  </Router>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
